236356d49e5fcb6608a901ad2dd29586c7e78c95
cid <cidemanuel2001@gmail.com>
2023-01-19 20:26:20 -0300

    Created Angular app. Added CORS support in the API. Added bootstrap and font-awesome.

--- API/Program.cs
+++ API/Program.cs
@@ -6,19 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 }
 
 //app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
