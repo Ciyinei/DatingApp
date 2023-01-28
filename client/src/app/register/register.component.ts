904bc32a09516fd2a43fad153cc48fcfc57c50f7
cid <cidemanuel2001@gmail.com>
2023-01-27 23:08:49 -0300

    Added components for elements in navbar. Added toastr service for notifications. Added route guard. Added a new theme.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class RegisterComponent {
   /**
    *
    */
-  constructor(private accountService:AccountService) {
+  constructor(private accountService:AccountService, private toastr: ToastrService) {
   }
 
   register() {
@@ -21,7 +22,9 @@ export class RegisterComponent {
       next: () => {
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toastr.error(error.error);
+      }
     })
   }
 
